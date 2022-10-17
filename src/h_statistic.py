b223e03668443ac5171fea0393b4af436d152be3
climate-policy-radar <climatepolicyradar@gmail.com>
2022-10-17 22:59:04 +0200

    feat: calculate hstat using our own partial dependence values implementation

--- src/h_statistic.py
+++ src/h_statistic.py
@@ -1,33 +1,32 @@
 from typing import List
 
 import pandas as pd
-from src.util.partial_dependence import find_index, partial_dependence_i_versus
+from src.util.partial_dependence import partial_dependence_value
+from tqdm import tqdm
+
 
 """
 
 TODO:
 1. Check correctness of H-statistic calculation
-2. Find more efficient way of calculating partial dependence profiles for OvO and OvA
-3. Wrap around more usable (less generic) function providing ability to create OvA and OvO summaries for all features
-4. Provide unit-tests for H-statistic
+2. Wrap around more usable (less generic) function providing ability to create OvA and OvO summaries for all features
+3. Provide unit-tests for H-statistic
 
 """
 
 
-# naive implementation of h statistic calculation (extremely slow)
-def calculate_h_stat_i_versus(model, X: pd.DataFrame, i: int, versus: List[int], class_number: int = 0) -> float:
-    pd_i, pd_versus, pd_i_versus = partial_dependence_i_versus(model, X, i, versus)
-
+def calculate_h_stat_i_versus(model, X: pd.DataFrame, i: str, versus: List[str]) -> float:
     nominator = denominator = 0
-    for index, row in X.iterrows():
-        i_index = find_index(pd_i_versus, 0, row[i])
-        versus_indexes = [find_index(pd_i_versus, column + 1, row[versus[column]]) for column in range(len(versus))]
+    for _, row in tqdm(X.iterrows()):
+        change_i = {i: row[i]}
+        change_versus = {col: row[col] for col in versus}
+        change_i_versus = {**change_i, **change_versus}
 
-        pd_i_versus_index = pd_i_versus['average'][class_number][i_index][tuple(versus_indexes)]
-        pd_i_index = pd_i['average'][class_number][i_index]
-        pd_versus_index = pd_versus['average'][class_number][tuple(versus_indexes)]
+        pd_i = partial_dependence_value(X, change_i, model.predict)
+        pd_versus = partial_dependence_value(X, change_versus, model.predict)
+        pd_i_versus = partial_dependence_value(X, change_i_versus, model.predict)
 
-        nominator += (pd_i_versus_index - pd_i_index - pd_versus_index) ** 2
-        denominator += pd_i_versus_index ** 2
+        nominator += (pd_i_versus - pd_i - pd_versus) ** 2
+        denominator += pd_i_versus**2
 
     return nominator / denominator
--- src/util/partial_dependence.py
+++ src/util/partial_dependence.py
@@ -1,15 +1,10 @@
+from typing import Dict, Callable
 import numpy as np
+import pandas as pd
 from sklearn.inspection import partial_dependence
 
 
-def find_index(pd_jk, column, value):
-    return np.where(pd_jk['values'][column] == value)[0][0]
-
-
-# naive execution of partial dependence
-def partial_dependence_i_versus(model, X, i, versus):
-    pd_i_versus = partial_dependence(model, X, [i] + versus, grid_resolution=len(X) + 1)
-    pd_i = partial_dependence(model, X, [i], grid_resolution=len(X) + 1)
-    pd_versus = partial_dependence(model, X, versus, grid_resolution=len(X) + 1)
-
-    return pd_i, pd_versus, pd_i_versus
+def partial_dependence_value(df: pd.DataFrame, change_dict: Dict, predict_function: Callable) -> float:
+    assert all(column in df.columns for column in change_dict.keys())
+    df_changed = df.assign(**change_dict)
+    return np.mean(predict_function(df_changed))
