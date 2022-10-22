ae6cce1867a7166ec1f1771a913a5f5305aa6c27
climate-policy-radar <climatepolicyradar@gmail.com>
2022-10-22 14:36:14 +0200

    fix: center pd values

--- src/h_statistic/calculation.py
+++ src/h_statistic/calculation.py
@@ -1,8 +1,9 @@
 from itertools import combinations
 from typing import List
 
+import numpy as np
 import pandas as pd
-from src.domain.domain import InteractionCalculationStrategy, ONE_VS_ONE, ONE_VS_ALL
+from src.domain.domain import ONE_VS_ALL, ONE_VS_ONE, InteractionCalculationStrategy
 from src.util.ops import remove_element, sample_if_not_none
 from src.util.partial_dependence import partial_dependence_value
 from tqdm import tqdm
@@ -10,17 +11,18 @@ from tqdm import tqdm
 """
 
 TODO:
-1. Check correctness of H-statistic calculation
-2. Provide unit-tests for H-statistic
+1. Provide unit-tests for H-statistic
 
 """
 
 
-def calculate_h_stat(model,
-                     X: pd.DataFrame,
-                     n: int = None,
-                     kind: InteractionCalculationStrategy = ONE_VS_ONE,
-                     show_progress: bool = False):
+def calculate_h_stat(
+    model,
+    X: pd.DataFrame,
+    n: int = None,
+    kind: InteractionCalculationStrategy = ONE_VS_ONE,
+    show_progress: bool = False,
+) -> pd.DataFrame:
     X_sampled = sample_if_not_none(X, n)
 
     if kind == ONE_VS_ONE:
@@ -31,36 +33,34 @@ def calculate_h_stat(model,
         raise ValueError(f"Interaction calculation strategy: {kind} unknown.")
 
 
-def calculate_h_stat_ovo(model, X: pd.DataFrame, progress) -> pd.DataFrame:
+def calculate_h_stat_ovo(model, X: pd.DataFrame, progress: bool) -> pd.DataFrame:
     pairs = list(combinations(X.columns, 2))
-    h_stat_pairs = [[c1, c2, calculate_h_stat_i_versus(model, X, c1, [c2], progress)] for c1, c2 in pairs if c1 != c2]
+    h_stat_pairs = [
+        [c1, c2, calculate_h_stat_i_versus(model, X, c1, [c2])]
+        for c1, c2 in tqdm(pairs, desc=ONE_VS_ONE.value, disable=not progress)
+    ]
 
-    return pd.DataFrame(h_stat_pairs,
-                        columns=["Feature 1", "Feature 2", "H-statistic"]).sort_values(by="H-statistic",
-                                                                                       ascending=False,
-                                                                                       ignore_index=True)
+    return pd.DataFrame(h_stat_pairs, columns=["Feature 1", "Feature 2", "H-statistic"]).sort_values(
+        by="H-statistic", ascending=False, ignore_index=True
+    )
 
 
 def calculate_h_stat_ova(model, X: pd.DataFrame, progress) -> pd.DataFrame:
-    h_stat_one_vs_all = [[column,
-                          calculate_h_stat_i_versus(model, X, column, remove_element(X.columns, column), progress)] for
-                         column in X.columns]
+    h_stat_one_vs_all = [
+        [column, calculate_h_stat_i_versus(model, X, column, remove_element(X.columns, column))]
+        for column in tqdm(X.columns, desc=ONE_VS_ALL.value, disable=not progress)
+    ]
 
-    return pd.DataFrame(h_stat_one_vs_all,
-                        columns=["Feature", "H-statistic"]).sort_values(by="H-statistic",
-                                                                        ascending=False,
-                                                                        ignore_index=True)
+    return pd.DataFrame(h_stat_one_vs_all, columns=["Feature", "H-statistic"]).sort_values(
+        by="H-statistic", ascending=False, ignore_index=True
+    )
 
 
-def calculate_h_stat_i_versus(model, X: pd.DataFrame, i: str, versus: List[str], progress: int = True) -> float:
-    nominator = denominator = 0
-
-    if progress:
-        iterator = tqdm(X.iterrows())
-    else:
-        iterator = X.iterrows()
-
-    for _, row in iterator:
+def calculate_h_stat_i_versus(model, X: pd.DataFrame, i: str, versus: List[str]) -> float:
+    pd_i_list = np.array([])
+    pd_versus_list = np.array([])
+    pd_i_versus_list = np.array([])
+    for _, row in X.iterrows():
         change_i = {i: row[i]}
         change_versus = {col: row[col] for col in versus}
         change_i_versus = {**change_i, **change_versus}
@@ -69,7 +69,14 @@ def calculate_h_stat_i_versus(model, X: pd.DataFrame, i: str, versus: List[str],
         pd_versus = partial_dependence_value(X, change_versus, model.predict)
         pd_i_versus = partial_dependence_value(X, change_i_versus, model.predict)
 
-        nominator += (pd_i_versus - pd_i - pd_versus) ** 2
-        denominator += pd_i_versus ** 2
+        pd_i_list = np.append(pd_i_list, pd_i)
+        pd_versus_list = np.append(pd_versus_list, pd_versus)
+        pd_i_versus_list = np.append(pd_i_versus_list, pd_i_versus)
 
-    return nominator / denominator
+    nominator = (
+        pd_i_versus_list - np.mean(pd_i_versus_list)
+        - (pd_i_list - np.mean(pd_i_list))
+        - (pd_versus_list - np.mean(pd_versus_list))
+    ) ** 2
+    denominator = (pd_i_versus_list - np.mean(pd_i_versus_list)) ** 2
+    return np.sum(nominator) / np.sum(denominator)
