4f5e7b70c234e97133f32f093253a9a60d8e84d0
krzyzinskim <mateusz_krzyzinski@wp.pl>
2022-11-26 22:50:42 +0100

    add split score interaction test

--- test/util.py
+++ test/util.py
@@ -3,6 +3,8 @@ import random
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
 
 CALIFORNIA_SUBSET = ["Longitude", "Latitude", "MedInc", "AveRooms"]
 SAMPLE_SIZE = 5
@@ -14,9 +16,18 @@ def california_housing_random_forest():
     california = fetch_california_housing()
     X = pd.DataFrame(california.data, columns=california.feature_names)
     y = california.target
-    model = RandomForestRegressor().fit(X, y)
-
+    model = RandomForestRegressor(n_estimators=10, max_depth=10).fit(X, y)
     return model, X, y
+
+def california_housing_boosting_models():
+    california = fetch_california_housing()
+    X = pd.DataFrame(california.data, columns=california.feature_names)
+    y = california.target
+    model_xgb = XGBRegressor(n_estimators=10, max_depth=4).fit(X, y)
+    model_lgbm = LGBMRegressor(n_estimators=10, max_depth=4).fit(X, y)
+    model_xgb_bis = XGBRegressor(n_estimators=40, max_depth=8).fit(X.iloc[:,:3], y)
+    model_lgbm_bis = LGBMRegressor(n_estimators=40, max_depth=8).fit(X.iloc[:,:3], y)
+    return model_xgb, model_lgbm, model_xgb_bis, model_lgbm_bis, X, y
 
 
 def toy_input():
