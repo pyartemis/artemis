aa9bd7baf5be89e4136647d6918803a227bc8655
pablo2811 <fijalkowskipablo@gmail.com>
2022-11-30 16:00:30 +0100

    add docstrings for MinimalDepthImportance

--- artemis/importance_methods/model_specific/_conditional_minimal_depth.py
+++ artemis/importance_methods/model_specific/_conditional_minimal_depth.py
@@ -10,15 +10,32 @@ from artemis.utilities.exceptions import FeatureImportanceWithoutInteractionExce
 
 
 class MinimalDepthImportance(VariableImportanceMethod):
+    """
+    Class implementing Minimal Depth Feature Importance. It applies to tree-based models.
+    It uses data calculated in Conditional Minimal Depth method and so needs to be calculated together.
+
+    Importance of a feature in the forest is defined as average distance to terminal nodes from the highest node using
+    this feature as a split variable.
+    """
 
     def __init__(self):
         super().__init__(ImportanceMethod.MINIMAL_DEPTH_IMPORTANCE)
 
     def importance(self,
                    model,  # to comply with the signature
-                   X: Optional[pd.DataFrame] = None,  # to comply with the signature
+                   X: Optional[pd.DataFrame] = None,
                    tree_id_to_depth_split: dict = None) -> pd.DataFrame:
+        """
+        Calculate minimal depth feature importance.
 
+        Args:
+            model:                      unused, comply with signature
+            X:                          data to use for calculating importance on
+            tree_id_to_depth_split:     precalculated in ConditionalMinimalDepth depths and split_variables map
+
+        Returns:
+            minimal depth feature importance
+        """
         _check_preconditions(self.method, tree_id_to_depth_split)
 
         columns = _make_column_dict(X)
