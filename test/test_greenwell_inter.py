a07ee14a0cf4c96351a283ea848b40a9fa6fdc95
climate-policy-radar <artur.zolkowski@wp.pl>
2022-12-07 00:31:00 +0100

    add classification tests with parameterized

--- test/test_greenwell_inter.py
+++ test/test_greenwell_inter.py
@@ -1,16 +1,32 @@
 import unittest
+from parameterized import parameterized_class
 
-from .util import california_housing_random_forest, has_decreasing_order, CALIFORNIA_SUBSET, SAMPLE_SIZE
+from .util import california_housing_random_forest, has_decreasing_order, CALIFORNIA_SUBSET, SAMPLE_SIZE, wine_random_forest, WINE_SUBSET
 from artemis.utilities.domain import InteractionMethod, VisualizationType
 from artemis.interactions_methods.model_agnostic import GreenwellMethod
 from artemis.utilities.exceptions import VisualizationNotSupportedException
 from artemis.visualizer._configuration import VisualizationConfigurationProvider
 
+MODEL_REG, X_REG, _ = california_housing_random_forest()
+MODEL_CLS, X_CLS, _ = wine_random_forest()
 
+
+@parameterized_class([
+    {
+        "model": MODEL_REG,
+        "X": X_REG,
+        "SUBSET": CALIFORNIA_SUBSET
+    },
+    {
+        "model": MODEL_CLS,
+        "X": X_CLS,
+        "SUBSET": WINE_SUBSET
+    },
+])
 class GreenwellMethodUnitTest(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.model, self.X, _ = california_housing_random_forest()
+    model = None
+    X = None
+    SUBSET = None
 
     def test_all_features_sampled(self):
         # when
@@ -20,7 +36,8 @@ class GreenwellMethodUnitTest(unittest.TestCase):
         # then
 
         # expected columns
-        self.assertSetEqual(set(greenwell_inter.ovo.columns), {"Feature 1", "Feature 2", InteractionMethod.VARIABLE_INTERACTION})
+        self.assertSetEqual(set(greenwell_inter.ovo.columns),
+                            {"Feature 1", "Feature 2", InteractionMethod.VARIABLE_INTERACTION})
 
         # sample size taken into account
         self.assertEqual(len(greenwell_inter.X_sampled), SAMPLE_SIZE)
@@ -31,13 +48,13 @@ class GreenwellMethodUnitTest(unittest.TestCase):
     def test_subset_of_features_sampled(self):
         # when
         greenwell_inter = GreenwellMethod()
-        greenwell_inter.fit(self.model, self.X, SAMPLE_SIZE, features=CALIFORNIA_SUBSET)
+        greenwell_inter.fit(self.model, self.X, SAMPLE_SIZE, features=self.SUBSET)
 
         # then
 
         # features parameter taken into account
         self.assertEqual(len(greenwell_inter.ovo), 6)
-        self.assertEqual(greenwell_inter.features_included, CALIFORNIA_SUBSET)
+        self.assertEqual(greenwell_inter.features_included, self.SUBSET)
 
         # sample size taken into account
         self.assertEqual(len(greenwell_inter.X_sampled), SAMPLE_SIZE)
@@ -56,10 +73,11 @@ class GreenwellMethodUnitTest(unittest.TestCase):
     def test_plot(self):
         # when
         greenwell_inter = GreenwellMethod()
-        greenwell_inter.fit(self.model, self.X, SAMPLE_SIZE, features=CALIFORNIA_SUBSET)
+        greenwell_inter.fit(self.model, self.X, SAMPLE_SIZE, features=self.SUBSET)
 
         # allowed plots are generated without exception
-        accepted_vis = VisualizationConfigurationProvider.get(InteractionMethod.VARIABLE_INTERACTION).accepted_visualizations
+        accepted_vis = VisualizationConfigurationProvider.get(
+            InteractionMethod.VARIABLE_INTERACTION).accepted_visualizations
         for vis in accepted_vis:
             greenwell_inter.plot(vis, show=False)
 
@@ -69,7 +87,7 @@ class GreenwellMethodUnitTest(unittest.TestCase):
     def test_should_raise_VisualizationNotSupportedException(self):
         # when
         greenwell_inter = GreenwellMethod()
-        greenwell_inter.fit(self.model, self.X, SAMPLE_SIZE, features=CALIFORNIA_SUBSET)
+        greenwell_inter.fit(self.model, self.X, SAMPLE_SIZE, features=self.SUBSET)
 
         # barchart is not supported for greenwell (no OvA), so this should raise VisualizationNotSupportedException
         with self.assertRaises(VisualizationNotSupportedException):
