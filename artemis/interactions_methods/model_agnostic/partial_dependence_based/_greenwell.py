ce4b906bc44c1de909d9c7c0f554c669a347e185
krzyzinskim <mateusz_krzyzinski@wp.pl>
2022-12-08 14:23:50 +0100

    add batch fit

--- artemis/interactions_methods/model_agnostic/partial_dependence_based/_greenwell.py
+++ artemis/interactions_methods/model_agnostic/partial_dependence_based/_greenwell.py
@@ -4,7 +4,8 @@ from typing import List, Optional
 import numpy as np
 import pandas as pd
 
-from artemis.utilities.domain import InteractionMethod
+from tqdm import tqdm
+from artemis.utilities.domain import InteractionMethod, ProgressInfoLog
 from artemis.utilities.ops import partial_dependence_value
 from ._pdp import PartialDependenceBasedMethod
 
@@ -30,21 +31,53 @@ class GreenwellMethod(PartialDependenceBasedMethod):
             random_state (int, optional) -- random state for reproducibility. Defaults to None."""
         super().__init__(InteractionMethod.VARIABLE_INTERACTION, random_state=random_state)
 
-    def _calculate_i_versus(self, predict_function, model, X_sampled: pd.DataFrame, i: str, versus: List[str]) -> float:
-        """
-        See `_calculate_i_versus` documentation in `PartialDependenceBasedMethod`.
-        Average of standard deviation over PDP profile for i-th, and j-th feature.
-        Specifics can be found in: https://arxiv.org/pdf/1805.04755.pdf
-        """
-        j = versus[0]  # only OvO
-        pd_values = np.array(
-            [
-                [partial_dependence_value(X_sampled, {i: x_i, j: x_j},
-                                                                       predict_function, model) for x_i in
-                 set(X_sampled[i])]
-                for x_j in set(X_sampled[j])
-            ]
-        )
-        res_j = np.apply_along_axis(stdev, 0, np.apply_along_axis(stdev, 1, pd_values))
-        res_i = np.apply_along_axis(stdev, 0, np.apply_along_axis(stdev, 0, pd_values))
-        return (res_j + res_i) / 2
+
+    def _ovo(self, predict_function, model, X_sampled: pd.DataFrame, show_progress: bool, batchsize: int):
+        full_pd = self._calculate_pdp_with_batch(model, predict_function, X_sampled, show_progress, batchsize)
+        value_pairs = []
+        for pair, pd_raw_values in full_pd.groupby(["Feature 1", "Feature 2"]):
+            pd_values = pd.crosstab(pd_raw_values["F1 value"], pd_raw_values["F2 value"], values=pd_raw_values["PD"], aggfunc=np.mean)
+            res_j = np.apply_along_axis(stdev, 0, np.apply_along_axis(stdev, 1, pd_values))
+            res_i = np.apply_along_axis(stdev, 0, np.apply_along_axis(stdev, 0, pd_values))
+            value_pairs.append([pair[0], pair[1], (res_j + res_i) / 2])
+        return pd.DataFrame(value_pairs, columns=["Feature 1", "Feature 2", self.method]).sort_values(
+            by=self.method, ascending=self.interactions_ascending_order, ignore_index=True
+        ).fillna(0)
+
+    def _calculate_pdp_with_batch(self, model, predict_function, X_sampled, show_progress, batchsize):
+        X_sampled_len = len(X_sampled)
+        pdp_dict = dict()
+        current_len = 0
+        range_dict = {}
+        X_full = pd.DataFrame()
+
+        for i, j in tqdm(self.pairs, desc=ProgressInfoLog.CALC_OVO, disable=not show_progress):
+            for x_i in set(X_sampled[i]):
+                for x_j in set(X_sampled[j]):
+                    change_dict = {i: x_i, j: x_j}
+                    X_changed = X_sampled.assign(**change_dict)
+                    range_dict[(i, j, x_i, x_j)] = (current_len, current_len + len(X_changed))
+                    current_len += X_sampled_len
+                    X_full = pd.concat((X_full, X_changed))
+                if current_len > batchsize:
+                    pdp_dict = _add_mean_preds_to_pdp_dict(model, predict_function, X_full, range_dict, pdp_dict)
+                    current_len = 0
+                    range_dict = {}
+                    X_full = pd.DataFrame()
+        if current_len > 0:
+            pdp_dict = _add_mean_preds_to_pdp_dict(model, predict_function, X_full, range_dict, pdp_dict)
+            current_len = 0
+            range_dict = {}
+            X_full = pd.DataFrame()
+
+        res = pd.DataFrame.from_dict(pdp_dict, orient='index', columns=['PD'])
+        res.index = pd.MultiIndex.from_tuples(tuples=res.index, names=['Feature 1', 'Feature 2', 'F1 value', 'F2 value'])
+        res = res.reset_index()
+        return res
+    
+def _add_mean_preds_to_pdp_dict(model, predict_function, X_full, range_dict, pdp_dict):
+    y = predict_function(model, X_full)
+    for i, j, x_i, x_j in range_dict.keys():
+        start, end = range_dict[(i, j, x_i, x_j)]
+        pdp_dict[(i, j, x_i, x_j)] = np.mean(y[start:end])
+    return pdp_dict
