d99227c04b60bed5cf80ae95d3c88dd9d9b2af63
krzyzinskim <mateusz_krzyzinski@wp.pl>
2022-12-10 15:01:08 +0100

    remove _calculate_pdp_with_batch from class

--- artemis/interactions_methods/model_agnostic/partial_dependence_based/_greenwell.py
+++ artemis/interactions_methods/model_agnostic/partial_dependence_based/_greenwell.py
@@ -33,7 +33,7 @@ class GreenwellMethod(PartialDependenceBasedMethod):
 
 
     def _ovo(self, predict_function, model, X_sampled: pd.DataFrame, show_progress: bool, batchsize: int):
-        full_pd = self._calculate_pdp_with_batch(model, predict_function, X_sampled, show_progress, batchsize)
+        full_pd = _calculate_pdp_with_batch(model, predict_function, X_sampled, show_progress, self.pairs, batchsize)
         value_pairs = []
         for pair, pd_raw_values in full_pd.groupby(["Feature 1", "Feature 2"]):
             pd_values = pd.crosstab(pd_raw_values["F1 value"], pd_raw_values["F2 value"], values=pd_raw_values["PD"], aggfunc=np.mean)
@@ -44,37 +44,37 @@ class GreenwellMethod(PartialDependenceBasedMethod):
             by=self.method, ascending=self.interactions_ascending_order, ignore_index=True
         ).fillna(0)
 
-    def _calculate_pdp_with_batch(self, model, predict_function, X_sampled, show_progress, batchsize):
-        X_sampled_len = len(X_sampled)
-        pdp_dict = dict()
+def _calculate_pdp_with_batch(model, predict_function, X_sampled, show_progress, pairs, batchsize):
+    X_sampled_len = len(X_sampled)
+    pdp_dict = dict()
+    current_len = 0
+    range_dict = {}
+    X_full = pd.DataFrame()
+
+    for i, j in tqdm(pairs, desc=ProgressInfoLog.CALC_OVO, disable=not show_progress):
+        for x_i in set(X_sampled[i]):
+            for x_j in set(X_sampled[j]):
+                change_dict = {i: x_i, j: x_j}
+                X_changed = X_sampled.assign(**change_dict)
+                range_dict[(i, j, x_i, x_j)] = (current_len, current_len + len(X_changed))
+                current_len += X_sampled_len
+                X_full = pd.concat((X_full, X_changed))
+            if current_len > batchsize:
+                pdp_dict = _add_mean_preds_to_pdp_dict(model, predict_function, X_full, range_dict, pdp_dict)
+                current_len = 0
+                range_dict = {}
+                X_full = pd.DataFrame()
+    if current_len > 0:
+        pdp_dict = _add_mean_preds_to_pdp_dict(model, predict_function, X_full, range_dict, pdp_dict)
         current_len = 0
         range_dict = {}
         X_full = pd.DataFrame()
 
-        for i, j in tqdm(self.pairs, desc=ProgressInfoLog.CALC_OVO, disable=not show_progress):
-            for x_i in set(X_sampled[i]):
-                for x_j in set(X_sampled[j]):
-                    change_dict = {i: x_i, j: x_j}
-                    X_changed = X_sampled.assign(**change_dict)
-                    range_dict[(i, j, x_i, x_j)] = (current_len, current_len + len(X_changed))
-                    current_len += X_sampled_len
-                    X_full = pd.concat((X_full, X_changed))
-                if current_len > batchsize:
-                    pdp_dict = _add_mean_preds_to_pdp_dict(model, predict_function, X_full, range_dict, pdp_dict)
-                    current_len = 0
-                    range_dict = {}
-                    X_full = pd.DataFrame()
-        if current_len > 0:
-            pdp_dict = _add_mean_preds_to_pdp_dict(model, predict_function, X_full, range_dict, pdp_dict)
-            current_len = 0
-            range_dict = {}
-            X_full = pd.DataFrame()
+    res = pd.DataFrame.from_dict(pdp_dict, orient='index', columns=['PD'])
+    res.index = pd.MultiIndex.from_tuples(tuples=res.index, names=['Feature 1', 'Feature 2', 'F1 value', 'F2 value'])
+    res = res.reset_index()
+    return res
 
-        res = pd.DataFrame.from_dict(pdp_dict, orient='index', columns=['PD'])
-        res.index = pd.MultiIndex.from_tuples(tuples=res.index, names=['Feature 1', 'Feature 2', 'F1 value', 'F2 value'])
-        res = res.reset_index()
-        return res
-    
 def _add_mean_preds_to_pdp_dict(model, predict_function, X_full, range_dict, pdp_dict):
     y = predict_function(model, X_full)
     for i, j, x_i, x_j in range_dict.keys():
